93cec7b21738d3e056b916962d17a862634be000
Eda <edacakmak92@gmail.com>
2023-07-09 19:44:55 +0300

    yanlis_yere_giden_kod.py

--- 19_01_22/yanlis_yere_giden_kod.py
+++ 19_01_22/yanlis_yere_giden_kod.py
@@ -4,8 +4,8 @@ import time
 time.sleep(5)
 
 def mesaj():
-    pyautogui.write("naber")
-    pyautogui.press('enter')
+    pyautogui.write("naber")  #The application that sends endless messages to the person we want on WhatsApp
+    pyautogui.press('enter')  #I have funny and unforgettable memories :DD
 
 
 while True:
